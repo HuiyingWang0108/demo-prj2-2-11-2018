97fdb96b7c0fd9b9d78aaa057bb901f0aae62307
Huiying0108 <whymissu1228@gmail.com>
2019-02-15 14:45:24 -0800

    server.js keep before

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var PORT = process.env.PORT || 8088;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 var passport = require('passport');
 var flash    = require('connect-flash');
